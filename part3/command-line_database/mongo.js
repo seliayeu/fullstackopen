195e1adfe79869617f1ac6b5ad98891fdecd3635
zloopo <dan.seliayeu@gmail.com>
2021-01-07 11:37:47 -0700

    add command-line_database

---
+++ part3/command-line_database/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3 || process.argv.length > 5 || process.argv.length === 4) {
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const url = 
+    `mongodb+srv://cooluser:${password}@cluster0.zab5w.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+const person = new Person({
+   name: process.argv[3],
+   number: process.argv[4]
+})
+
+if (process.argv.length === 5) { 
+    person.save().then(result => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    console.log("phonebook:")
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
